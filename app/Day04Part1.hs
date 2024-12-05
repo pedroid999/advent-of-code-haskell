5474c4b545d639332fafcf076f307efc1eb170fc
Pedro Nieto Méndez <pedro.nieto@sngular.com>
2024-12-05 23:39:14 +0100

    Day 4 part 1, port from java version.

---
+++ app/Day04Part1.hs
@@ -0,0 +1,53 @@
+module Main where
+
+import Data.Array
+
+-- Constants
+xmas :: String
+xmas = "XMAS"
+
+offsets :: [(Int, Int)]
+offsets = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
+
+-- Main function
+main :: IO ()
+main = do
+    contents <- readFile "resources/inputs/day04.txt"
+    let input = lines contents
+    let matrix = listToArray2D input
+    print $ solveInput matrix
+
+-- Convert list to 2D Array
+listToArray2D :: [String] -> Array (Int, Int) Char
+listToArray2D xs =
+    array
+        ((0, 0), (rows - 1, cols - 1))
+        [((i, j), (xs !! i) !! j) | i <- [0 .. rows - 1], j <- [0 .. cols - 1]]
+  where
+    rows = length xs
+    cols = length (head xs)
+
+-- Main solving function
+solveInput :: Array (Int, Int) Char -> Int
+solveInput matrix = sum [starCmp matrix (i, j) | i <- [0 .. rows], j <- [0 .. cols]]
+  where
+    (rows, cols) = snd $ bounds matrix
+
+-- Check all directions for XMAS pattern
+starCmp :: Array (Int, Int) Char -> (Int, Int) -> Int
+starCmp matrix pos = length $ filter id [starRayCmp matrix pos offset 0 | offset <- offsets]
+
+-- Check single direction for XMAS pattern
+starRayCmp :: Array (Int, Int) Char -> (Int, Int) -> (Int, Int) -> Int -> Bool
+starRayCmp matrix (posF, posC) (offsetF, offsetC) xmasPos
+    | not (inBounds matrix (posF, posC)) = False
+    | matrix ! (posF, posC) /= xmas !! xmasPos = False
+    | xmasPos == length xmas - 1 = True
+    | otherwise = starRayCmp matrix (posF + offsetF, posC + offsetC) (offsetF, offsetC) (xmasPos + 1)
+
+-- Helper function to check if position is within bounds
+inBounds :: Array (Int, Int) Char -> (Int, Int) -> Bool
+inBounds matrix (row, col) =
+    row >= minRow && row <= maxRow && col >= minCol && col <= maxCol
+  where
+    ((minRow, minCol), (maxRow, maxCol)) = bounds matrix
